e8c0b9e47fcc765cc93a7c6740159133d71db022
Mindy Mallory (Admin) <mlmalloradmin@ag-econ-0009wt.BoilerAD.Purdue.edu>
2020-01-24 16:58:49 -0500

    dfdfsfsdf

---
+++ RollFutures-overshoot.R
@@ -0,0 +1,64 @@
+#Download data
+
+library(tidyverse)
+library(profvis)
+
+
+
+#apikey <- 'putyourapikeyhere'
+
+
+tick_<- list('1959':'2012')[[1]] 
+
+
+#C
+c_contracts <- c('H', 'K', 'N', 'U', 'Z')
+c_months    <- c('03', '05', '07', '09', '12')
+
+#S
+s_contracts <- c( 'F', 'H', 'K', 'N', 'Q', 'U', 'X')
+s_months    <- c('01', '03', '05', '07', '09', '11')
+
+#W
+w_contracts <- c( 'H', 'K', 'N', 'U', 'Z')
+w_months    <- c_months
+
+#Years Loop
+for(j in 1:length(tick_)){
+  
+  # Corn Contracts
+  for(i in 1:length(c_contracts)){
+    
+    tick <- tick_[[j]]
+    
+    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZC', c_contracts[i], 
+                   substr(as.character(tick), 3, 4), '&type=daily') %>%
+      download.file(destfile =paste0('data/', 'ZC', c_contracts[i], tick, '.csv'), method = "libcurl")
+    
+  }
+  
+  # Soybeans
+  for(i in 1:length(s_contracts)){
+    
+    tick <- tick_[[j]]
+    
+    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZS', s_contracts[i], 
+                   substr(as.character(tick), 3, 4), '&type=daily') %>%
+      download.file(destfile =paste0('data/', 'ZS', s_contracts[i], tick, '.csv'), method = "libcurl")
+    
+  }
+
+  # Chi Wheat
+  for(i in 1:length(w_contracts)){
+    
+    tick <- tick_[[j]]
+    
+    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZW', w_contracts[i], 
+                   substr(as.character(tick), 3, 4), '&type=daily') %>%
+      download.file(destfile =paste0('data/', 'ZW', w_contracts[i], tick, '.csv'), method = "libcurl")
+    
+  }
+  
+  
+}
+
