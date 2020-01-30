a93a0c352912c85bce7e3aa8e06b67d5be69064b
Mindy <mlmallor@purdue.edu>
2020-01-29 19:59:10 -0500

    dfdfsdfv

--- RollFutures-overshoot.R
+++ RollFutures-overshoot.R
@@ -8,7 +8,7 @@ library(profvis)
 #apikey <- 'putyourapikeyhere'
 
 
-tick_<- list('1959':'2012')[[1]] 
+tick_<- list('1983':'2012')[[1]] 
 
 
 #C
@@ -23,39 +23,54 @@ s_months    <- c('01', '03', '05', '07', '09', '11')
 w_contracts <- c( 'H', 'K', 'N', 'U', 'Z')
 w_months    <- c_months
 
+# CL
+cl_contracts <- c('F', 'H', 'K', 'N', 'Q', 'U', 'X', 'Z') # Chose contract months that match c and s
+cl_months <- c('01', '03', '05', '07', '09', '11', '12')
+
 #Years Loop
 for(j in 1:length(tick_)){
+  # 
+  # # Corn Contracts
+  # for(i in 1:length(c_contracts)){
+  #   
+  #   tick <- tick_[[j]]
+  #   
+  #   data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZC', c_contracts[i], 
+  #                  substr(as.character(tick), 3, 4), '&type=daily') %>%
+  #     download.file(destfile =paste0('data/', 'ZC', c_contracts[i], tick, '.csv'), method = "libcurl")
+  #   
+  # }
+  # 
+  # # Soybeans
+  # for(i in 1:length(s_contracts)){
+  #   
+  #   tick <- tick_[[j]]
+  #   
+  #   data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZS', s_contracts[i], 
+  #                  substr(as.character(tick), 3, 4), '&type=daily') %>%
+  #     download.file(destfile =paste0('data/', 'ZS', s_contracts[i], tick, '.csv'), method = "libcurl")
+  #   
+  # }
+  # 
+  # # Chi Wheat
+  # for(i in 1:length(w_contracts)){
+  #   
+  #   tick <- tick_[[j]]
+  #   
+  #   data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZW', w_contracts[i], 
+  #                  substr(as.character(tick), 3, 4), '&type=daily') %>%
+  #     download.file(destfile =paste0('data/', 'ZW', w_contracts[i], tick, '.csv'), method = "libcurl")
+  #   
+  # }
   
-  # Corn Contracts
-  for(i in 1:length(c_contracts)){
+  # WTI Crude
+  for(i in 1:length(cl_contracts)){
     
     tick <- tick_[[j]]
     
-    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZC', c_contracts[i], 
+    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=CL', cl_contracts[i], 
                    substr(as.character(tick), 3, 4), '&type=daily') %>%
-      download.file(destfile =paste0('data/', 'ZC', c_contracts[i], tick, '.csv'), method = "libcurl")
-    
-  }
-  
-  # Soybeans
-  for(i in 1:length(s_contracts)){
-    
-    tick <- tick_[[j]]
-    
-    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZS', s_contracts[i], 
-                   substr(as.character(tick), 3, 4), '&type=daily') %>%
-      download.file(destfile =paste0('data/', 'ZS', s_contracts[i], tick, '.csv'), method = "libcurl")
-    
-  }
-
-  # Chi Wheat
-  for(i in 1:length(w_contracts)){
-    
-    tick <- tick_[[j]]
-    
-    data <- paste0('http://ondemand.websol.barchart.com/getHistory.csv?apikey=', apikey, '&symbol=ZW', w_contracts[i], 
-                   substr(as.character(tick), 3, 4), '&type=daily') %>%
-      download.file(destfile =paste0('data/', 'ZW', w_contracts[i], tick, '.csv'), method = "libcurl")
+      download.file(destfile =paste0('data/', 'CL', cl_contracts[i], tick, '.csv'), method = "libcurl")
     
   }
   
